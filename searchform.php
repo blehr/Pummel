30a5e36af2a974bb0a10f89b0861f441a95ab39c
Brandon Lehr <blehr.mail@gmail.com>
2015-04-24 23:55:39 +0000

    Cleanup searchform

--- searchform.php
+++ searchform.php
@@ -1,8 +1,8 @@
 <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
 	<label>
-		<span class="screen-reader-text"><?php echo _x( 'Search for:', 'label' ) ?></span>
+		<span class="screen-reader-text"><?php echo __( 'Search for:', 'bpl' ) ?></span>
 	</label>
-		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search …', 'placeholder' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Looking for something?', 'label' ) ?>" />
+		<input type="search" class="search-field" placeholder="<?php echo esc_attr__( 'Search …', 'bpl' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr__( 'Looking for something?', 'bpl' ) ?>" />
 	
-	<input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>" />
+	<input type="submit" class="search-submit" value="<?php echo esc_attr__( 'Search', 'bpl' ) ?>" />
 </form>
