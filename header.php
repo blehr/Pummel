56382aa5c3bcd8a431475021a2aa980c77d3798b
Brandon Lehr <blehr.mail@gmail.com>
2015-05-12 03:06:57 +0000

    Remove favicon link, better left for plugin

--- header.php
+++ header.php
@@ -14,7 +14,7 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
-<link rel="icon" href="<?php echo esc_url( get_template_directory_uri() ); ?>/images/favicon.ico">
+
 
 <?php wp_head(); ?>
 </head>
